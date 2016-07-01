733ada51b87060ddab4f9155ab0b14099411c50a
silverweed <silverweed1991@gmail.com>
2016-07-01 15:26:51 +0200

    add android branch

---
+++ app/src/main/java/codecup/qu/MainActivity.java
@@ -0,0 +1,20 @@
+package codecup.qu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView)findViewById(R.id.gameview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
---
+++ emu.sh
@@ -0,0 +1,1 @@
+~/Android/Sdk/tools/emulator -netdelay none -netspeed full -avd Nexus_5X_API_22 -use-system-libs
